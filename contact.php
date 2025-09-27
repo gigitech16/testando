8cc11983fd8212c53467980087478d7b213421b3
gigitech16 <giovannaduartebraga@hotmail.com>
2025-09-27 11:05:53 -0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -6,8 +6,8 @@
   * For more info and help: https://bootstrapmade.com/php-email-form/
   */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  // Substitua 'contact@example.com' pelo seu endereço de e-mail da Chocoarte
+  $receiving_email_address = 'contato@chocoarte.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -23,19 +23,19 @@
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  // Opcional: Descomente e configure as credenciais SMTP se quiser usar um servidor SMTP para enviar e-mails
   /*
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'exemplo.com.br',
+    'username' => 'seu_usuario_smtp',
+    'password' => 'sua_senha_smtp',
     'port' => '587'
   );
   */
 
-  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['name'], 'De');
   $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  $contact->add_message( $_POST['message'], 'Mensagem', 10);
 
   echo $contact->send();
 ?>
